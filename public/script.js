e7dcb134869d729f054f0e57933afd67c7e0a150
Zept0mc <kuba.kuczera11@gmail.com>
2024-04-15 21:00:40 +0200

    Css and basic js

---
+++ public/script.js
@@ -0,0 +1,17 @@
+const myForm = document.querySelector("#myform")
+const loginInput = myForm.login;
+const btn = document.querySelector("#btn")
+const loginDiv = document.querySelector("#loginDiv")
+
+myForm.addEventListener("submit", e=>{
+    e.preventDefault();
+    if(loginInput.value.trim().length != 0){
+        loginDiv.style.visibility = "hidden";
+        let login = loginInput.value.trim();
+        location.href = './game.html';
+    }else{
+        alert("Wpisz nazwę użytkownika!!!");
+        location.reload();
+    }
+    
+})
